80973fc10929aa8b9afb8371750baa6fdd0a016b
Laura Stevenson <laura.j.stevenson05@gmail.com>
2023-12-13 14:05:55 -0700

    first commit

---
+++ src/main/java/com/example/bugwarshealerbackend/BugwarsHealerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bugwarshealerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugwarsHealerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugwarsHealerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bugwarshealerbackend/BugwarsHealerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bugwarshealerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugwarsHealerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
