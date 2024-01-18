0c53cb49f5f1b40ba52294651638aea96096aac9
Ashley Mical <ashleymmical@gmail.com>
2024-01-18 14:21:42 -0600

    update logout controller to postmapping

--- src/main/java/com/example/bugwarshealerbackend/controller/LogoutController.java
+++ src/main/java/com/example/bugwarshealerbackend/controller/LogoutController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class LogoutController {
-    @GetMapping("/logout")
+    @PostMapping("/logout")
     public void logout()
     {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
