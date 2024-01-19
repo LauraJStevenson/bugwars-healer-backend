8f864d620c45a0ed6dbb2291971746816fb4b78c
Laura Stevenson <laura.j.stevenson05@gmail.com>
2024-01-19 15:48:35 -0700

    added 'CREATE TABLE IF NOT EXISTS users' to data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,14 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(15) NOT NULL,
+    firstname VARCHAR(15) NOT NULL,
+    lastname VARCHAR(15) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    counter INTEGER,
+    activated BOOLEAN
+);
+
 INSERT INTO users (username, firstname, lastname, password_hash, email)
 VALUES
 ('TestLaura', 'Laura', 'Stevenson', '12345', 'laura@mail.com'),
