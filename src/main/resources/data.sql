2639cdccd99ed21f6fb12e49dbd8e74e6e48d14c
Laura Stevenson <laura.j.stevenson05@gmail.com>
2024-01-19 15:01:42 -0700

    added data.sql file to create and populate table 'users' in remote database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, firstname, lastname, password_hash, email)
+VALUES
+('TestLaura', 'Laura', 'Stevenson', '12345', 'laura@mail.com'),
+('TestAshley', 'Ashley', 'Mical', '45678', 'ashley@mail.com');
