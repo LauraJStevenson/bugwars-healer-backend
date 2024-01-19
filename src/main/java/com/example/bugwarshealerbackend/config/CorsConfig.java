2818461b59a57de00f80cc686643254170832c23
Laura Stevenson <laura.j.stevenson05@gmail.com>
2024-01-19 12:54:16 -0700

    Added 'Access-Control-Allow-Origin' & 'Access-Control-Allow-Credentials' to addCorsMappings

--- src/main/java/com/example/bugwarshealerbackend/config/CorsConfig.java
+++ src/main/java/com/example/bugwarshealerbackend/config/CorsConfig.java
@@ -11,9 +11,10 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         System.out.println("CORS configuration applied!");
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "https://stage-bugwars-healer.onrender.com")
+                .allowedOrigins("http://localhost:5173", "https://bugwars-healer-frontend-stage.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(true)
+                        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
     }
 }
