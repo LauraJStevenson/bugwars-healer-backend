00279dddad280ba6196a6b7e3928b93d6e19ce2d
Kimlyn De Lara <klyndelara@gmail.com>
2024-01-30 23:44:59 -0800

    finish jwtService test

--- src/main/java/com/example/bugwarshealerbackend/controller/LoginController.java
+++ src/main/java/com/example/bugwarshealerbackend/controller/LoginController.java
@@ -23,19 +23,19 @@ public class LoginController {
     @PostMapping("/login")
     public ResponseEntity<AuthenticatedUser> login (@Valid @RequestBody UserLogin userlogin) {
 
-        User loggedInUser = userRepository.findByUsername(userlogin.getUsername());
+        User loggedInUser = userRepository.findByUsername(userlogin.getUsername()); //retrieves a user from the repository based on the provided username in the UserLogin object.
         if (loggedInUser == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null); //checks if the user is not found, it returns a 401 Unauthorized status with an empty response body.
         }
 
         // Salt checkpoint
-        boolean passwordsMatch = BCrypt.checkpw(userlogin.getPassword() /*123456*/, loggedInUser.getPassword() /* fhdjskhfd;asjkfsdf62538727732 */);
-        if (!passwordsMatch) {
+        boolean passwordsMatch = BCrypt.checkpw(userlogin.getPassword() /*123456*/, loggedInUser.getPassword() /* fhdjskhfd;asjkfsdf62538727732 */); //Compares the password provided in the UserLogin object with the stored hash pw of the retrieved user.
+        if (!passwordsMatch) { //checks if passwords match. If not it returns a 401 Unauthorize status with an empty response body.
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
         }
 
-        String jwtToken = JwtService.createToken(userlogin.getUsername());
+        String jwtToken = JwtService.createToken(userlogin.getUsername()); //Generates a JWT using the JwtService.createToken method containing information of the username
         return ResponseEntity.ok(new AuthenticatedUser(loggedInUser.getId(), loggedInUser.getUsername(), loggedInUser.getPassword(), loggedInUser.getFirstname(), loggedInUser.getLastname(), loggedInUser.getEmail(), jwtToken));
-
+        //if the login is successful, it returns a 200 status with the AuthenticatedUser object which will contain the information of the authenticated user and token.
     }
 }
--- src/test/java/com/example/bugwarshealerbackend/jwt/JwtServiceTest.java
+++ src/test/java/com/example/bugwarshealerbackend/jwt/JwtServiceTest.java
@@ -1,10 +1,20 @@
 package com.example.bugwarshealerbackend.jwt;
 
+import com.zaxxer.hikari.pool.HikariProxyCallableStatement;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
+import java.security.Key;
+import java.util.Date;
+
+import static com.example.bugwarshealerbackend.jwt.JwtService.*;
+import static org.aspectj.bridge.Version.getTime;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JwtServiceTest {
@@ -14,6 +24,7 @@ class JwtServiceTest {
     }
 
     public static String ISSUER = "com.example.bugwarsbackend";
+
     @Test
     public void createToken_shouldGenerateValidToken() {
         String oldSecret = JwtService.SECRET_KEY;
@@ -23,7 +34,7 @@ class JwtServiceTest {
 
 
             String username = "testUser";
-            String token = JwtService.createToken(username);
+            String token = createToken(username);
 
 
             Jws<Claims> claimsJws = Jwts.parserBuilder()
@@ -33,10 +44,69 @@ class JwtServiceTest {
             assertEquals(username, claimsJws.getBody().getSubject());
             assertEquals(ISSUER, claimsJws.getBody().getIssuer());
 
-        }
-        finally {
+        } finally {
             JwtService.SECRET_KEY = oldSecret;
         }
     }
 
+
+    @Test
+    public void getUserNameForTokenTest() {
+
+        String oldSecret = JwtService.SECRET_KEY;
+        try {
+            String secret = "a2ltbHluXyZeKigmKGFuZComJSYqP3lhZ211cjIwMjQ=";
+            JwtService.SECRET_KEY = secret;
+
+            String username = "testUser";
+
+            String token = JwtService.createToken(username);
+
+            String result = JwtService.getUserNameForToken(token);
+            assertEquals(username, result);
+        } finally {
+            JwtService.SECRET_KEY = oldSecret;
+        }
+    }
+
+    @Test
+    public void getUserNameForTokenTest_wrongIssuer() {
+
+        String oldSecret = JwtService.SECRET_KEY;
+        try {
+            String secret = "a2ltbHluXyZeKigmKGFuZComJSYqP3lhZ211cjIwMjQ=";
+            JwtService.SECRET_KEY = secret;
+
+            String username = "testUser";
+
+            String token = JwtService.createToken(username, "wrongIssuer");
+
+            String result = JwtService.getUserNameForToken(token);
+            assertNull(result);
+        } finally {
+            JwtService.SECRET_KEY = oldSecret;
+        }
+    }
+
+    @Test
+    public void getUserNameForTokenTest_expiredToken() {
+
+        String oldSecret = JwtService.SECRET_KEY;
+        try {
+            String secret = "a2ltbHluXyZeKigmKGFuZComJSYqP3lhZ211cjIwMjQ=";
+            JwtService.SECRET_KEY = secret;
+            String username = "testUser";
+
+            String token = JwtService.createToken("testUser", String.valueOf(new Date(System.currentTimeMillis() - 1000)));
+
+            String result = JwtService.getUserNameForToken(token);
+            assertNull(result);
+
+        } finally {
+            JwtService.SECRET_KEY = oldSecret;
+        }
+
+    }
 }
+
+
