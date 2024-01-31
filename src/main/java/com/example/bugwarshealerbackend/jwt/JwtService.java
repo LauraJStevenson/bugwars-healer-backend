8a45ff15754f2afc9b1739875e9896c7b9c261a6
Ashley Mical <108195754+micamash@users.noreply.github.com>
2024-01-31 10:48:49 -0600

    added Service annotation to JwtService

--- src/main/java/com/example/bugwarshealerbackend/jwt/JwtService.java
+++ src/main/java/com/example/bugwarshealerbackend/jwt/JwtService.java
@@ -6,12 +6,14 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.UUID;
 
+@Service
 public class JwtService {
 
     public static String ISSUER = "com.example.bugwarsbackend";
