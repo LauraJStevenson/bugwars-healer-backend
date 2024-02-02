6e004f0176a1ff8d83d657a58f2eb05e76ca64c1
Yagmur Muslu <ygmrmuslu@outlook.com>
2024-02-02 15:36:54 -0800

    Fix issues related to using h2 database.

--- src/test/java/com/example/bugwarshealerbackend/controller/UserControllerTest.java
+++ src/test/java/com/example/bugwarshealerbackend/controller/UserControllerTest.java
@@ -9,6 +9,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,7 +20,8 @@ import org.springframework.web.context.WebApplicationContext;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class UserControllerTest {
 
     @Autowired
