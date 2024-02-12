54cbd681647b0415cedf663945aedeb5f25fa7ae
Yagmur Muslu <ygmrmuslu@outlook.com>
2024-02-12 14:33:31 -0800

    Compiler improvements
    Added support for HashTags, empty lines, and variable space count while validating and compiling scripts.

--- src/test/java/com/example/bugwarshealerbackend/game/CompilerTest.java
+++ src/test/java/com/example/bugwarshealerbackend/game/CompilerTest.java
@@ -12,6 +12,109 @@ import org.springframework.test.context.ActiveProfiles;
 public class CompilerTest {
 
     @Test
+    public void compiler_cleanScript() {
+        //Arrange
+        String script = """
+# Head Spinner
+:ANOTHER_LABEL
+:START       att
+             att  #I'm a comment
+             ifFood EAT
+             mov
+             rotr
+             rotl
+             goto START
+:EAT         eat
+             rotr
+             rotr
+             goto START
+             
+#I'm a comment""";
+        List<String> expectedResult = new ArrayList<>();
+        expectedResult.add(":ANOTHER_LABEL");
+        expectedResult.add(":START att");
+        expectedResult.add("att");
+        expectedResult.add("ifFood EAT");
+        expectedResult.add("mov");
+        expectedResult.add("rotr");
+        expectedResult.add("rotl");
+        expectedResult.add("goto START");
+        expectedResult.add(":EAT eat");
+        expectedResult.add("rotr");
+        expectedResult.add("rotr");
+        expectedResult.add("goto START");
+        //Act
+        List<String> result = Compiler.cleanScript(script);
+        //Assert
+        Assert.assertArrayEquals(expectedResult.toArray(new String[0]),result.toArray(new String[0]));
+    }
+    @Test
+    public void compiler_is_valid_forCleanScript() {
+        //Arrange
+        String script = """
+# Head Spinner
+:START       att
+             att  #I'm a comment
+             ifFood EAT
+             mov
+             rotr
+             rotl
+             goto START
+:EAT         eat
+             rotr
+             rotr
+             goto START
+             
+#I'm a comment""";
+        //Act
+        boolean isValid = Compiler.validate(script);
+        //Assert
+        Assert.assertTrue(isValid);
+    }
+
+    @Test
+    public void compiler_compiled_for_cleanScript() {
+        //Arrange
+        List<Integer> expectedResult = new ArrayList<>();
+        expectedResult.add(13);
+        expectedResult.add(13);
+        expectedResult.add(32);
+        expectedResult.add(9);
+        expectedResult.add(10);
+        expectedResult.add(11);
+        expectedResult.add(12);
+        expectedResult.add(35);
+        expectedResult.add(0);
+        expectedResult.add(14);
+        expectedResult.add(11);
+        expectedResult.add(11);
+        expectedResult.add(35);
+        expectedResult.add(0);
+
+        String script = """
+# Head Spinner
+:START       att
+             att  #I'm a comment
+             ifFood EAT
+             mov
+             rotr
+             rotl
+             goto START
+:EAT         eat
+             rotr
+             rotr
+             goto START
+             
+#I'm a comment""";
+        //Act
+        List<Integer> compiledScript = Compiler.compile(script);
+        //Assert
+        for(int i = 0; i <compiledScript.size(); i++) {
+            Assert.assertEquals(expectedResult.get(i),compiledScript.get(i));
+        }
+    }
+
+    @Test
     public void compiler_is_valid() {
         //Arrange
         String script = """
@@ -265,9 +368,6 @@ goto START""";
         //Act
         List<Integer> compiledScript = Compiler.compile(script);
         //Assert
-        for(int i = 0; i <compiledScript.size(); i++) {
-            Assert.assertEquals(expectedResult.get(i),compiledScript.get(i));
-        }
+        Assert.assertArrayEquals(expectedResult.toArray(new Integer[0]),compiledScript.toArray(new Integer[0]));
     }
-
 }
