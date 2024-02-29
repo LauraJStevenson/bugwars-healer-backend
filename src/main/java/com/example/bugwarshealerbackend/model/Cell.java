0a511f6daee84244450bf07dfc9a4617b341a2e4
Yagmur Muslu <ygmrmuslu@outlook.com>
2024-02-28 19:00:05 -0800

    implemented bug move logic

--- src/main/java/com/example/bugwarshealerbackend/model/Cell.java
+++ src/main/java/com/example/bugwarshealerbackend/model/Cell.java
@@ -1,4 +1,18 @@
 package com.example.bugwarshealerbackend.model;
 
-public class Cell {
+public abstract class Cell {
+    public abstract Cell clone();
+
+    private int x;
+    private int y;
+
+    protected Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    protected int getX() {return x;}
+    protected int getY() {return y;}
+    protected void setX(int x) {this.x = x;}
+    protected void setY(int y) {this.y = y;}
 }
--- src/main/java/com/example/bugwarshealerbackend/model/GameMap.java
+++ src/main/java/com/example/bugwarshealerbackend/model/GameMap.java
@@ -3,6 +3,11 @@ package com.example.bugwarshealerbackend.model;
 public class GameMap {
 
     private Cell[][] cells;
+
+    private GameMap(Cell[][] cells) {
+        this.cells = cells;
+    }
+
     public GameMap(String asciiMap) {
         String[] lines = asciiMap.split("\n");
         for(int i = 0; i < lines.length; i++) {
@@ -13,24 +18,43 @@ public class GameMap {
                 // create current cell
                 switch (character) {
                     case 'X' :
-                        currentCell = new Wall();
+                        currentCell = new Wall(i,j);
                         break;
                     case ' ' :
-                        currentCell = new EmptySpace();
+                        currentCell = new EmptySpace(i,j);
                         break;
                     case 'a' :
                     case 'b' :
                     case 'c' :
                     case 'd' :
-                        currentCell = new Bug();
+                        currentCell = new Bug(i,j);
                         break;
                     case 'f' :
-                        currentCell = new Food();
+                        currentCell = new Food(i,j);
                         break;
                     default: throw new RuntimeException("Unexpected character in serialized map!");
                 }
                 cells[i][j] = currentCell;
             }
         }
+    }
+
+    // Get all cells
+    public Cell[][] getCells () {
+        return this.cells;
+    }
+
+    //Copy gameMap method
+    public GameMap clone() {
+        Cell[][] cells = new Cell[this.cells.length][];
+        //copy each cell and create new area
+        for(int i = 0 ; i< cells.length; i++) {
+            cells[i] = new Cell[this.cells[i].length];
+            for(int j = 0 ; j < cells[i].length; j++ ) {
+                cells[i][j] = this.cells[i][j].clone();
+            }
+        }
+        GameMap newMap = new GameMap(cells);
+        return newMap;
     }
 }
