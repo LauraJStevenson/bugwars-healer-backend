cc9f16ec258c3ddda6f0753b1f5394d830e0392a
Ashley Mical <108195754+micamash@users.noreply.github.com>
2024-03-01 11:33:45 -0600

    update map controller

--- src/main/java/com/example/bugwarshealerbackend/controller/MapController.java
+++ src/main/java/com/example/bugwarshealerbackend/controller/MapController.java
@@ -3,16 +3,27 @@ package com.example.bugwarshealerbackend.controller;
 import com.example.bugwarshealerbackend.jpa.MapRepository;
 import com.example.bugwarshealerbackend.model.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@CrossOrigin
 public class MapController {
 
     @Autowired
     private MapRepository mapRepository;
     @GetMapping("/maps")
-    public List<Map> maps () {
-        return mapRepository.findAll();
+    public ResponseEntity<List<Map>> maps() {
+        try {
+            List<Map> maps = mapRepository.findAll();
+            return ResponseEntity.ok(maps);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
