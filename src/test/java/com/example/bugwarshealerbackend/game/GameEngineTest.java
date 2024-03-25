6ac9e7b42b3d7e8eed26218d8baa8c1d99d33b76
Yagmur Muslu <ygmrmuslu@outlook.com>
2024-03-24 23:08:28 -0700

    create game engine test eat food and create new same bug

--- src/test/java/com/example/bugwarshealerbackend/game/GameEngineTest.java
+++ src/test/java/com/example/bugwarshealerbackend/game/GameEngineTest.java
@@ -1,6 +1,7 @@
 package com.example.bugwarshealerbackend.game;
 
 import com.example.bugwarshealerbackend.model.Bug;
+import com.example.bugwarshealerbackend.model.Food;
 import com.example.bugwarshealerbackend.model.GameMap;
 import org.junit.Assert;
 import org.junit.Test;
@@ -43,7 +44,6 @@ XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"""
             Assert.assertEquals(bug.getColumn(), nextMapBug.getColumn());
             Assert.assertNotEquals(bug.getDirection(),nextMapBug.getDirection());
         }
-
     }
     @Test
     public void game_engine_bug_noop_move() {
@@ -153,7 +153,49 @@ XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"""
 
     @Test
     public void game_engine_check_new_bug_after_eat_food(){
+        //Arrange
+        GameMap map = new GameMap(
+"""
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+X            f              X
+X            a              X
+X    f                f     X
+X    b                c     X
+X            f              X
+X            d              X
+X                           X
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"""
+        );
+        //Act
+        int[] script = new int[2];
+        script[0] = 14;
+        script[1] = 11;
+        List<GameMap> replay = GameEngine.play(map, script, script, script, script, 2);
+        //Assert
+        Assert.assertEquals(3, replay.size());
+        GameMap initialMap = replay.get(0);
+        Assert.assertEquals(map, initialMap);
+        GameMap nextMap = replay.get(2);
+        List<Bug> bugs = map.getBugs();
+        List<Bug> nextMapBugs = nextMap.getBugs();
+        Assert.assertEquals(8, nextMapBugs.size());
+        // assert the initial bugs
+        assertBug(nextMapBugs.get(1), bugs.get(0).getRow(), bugs.get(0).getColumn(), 'E', bugs.get(0).getBugType());
+        assertBug(nextMapBugs.get(4), bugs.get(1).getRow(), bugs.get(1).getColumn(), 'E', bugs.get(1).getBugType());
+        assertBug(nextMapBugs.get(5), bugs.get(2).getRow(), bugs.get(2).getColumn(), 'E', bugs.get(2).getBugType());
+        assertBug(nextMapBugs.get(7), bugs.get(3).getRow(), bugs.get(3).getColumn(), 'E', bugs.get(3).getBugType());
+        // assert the new bugs
+        assertBug(nextMapBugs.get(0), 1, 13, 'N', 'a');
+        assertBug(nextMapBugs.get(2), 3, 5, 'N', 'b');
+        assertBug(nextMapBugs.get(3), 3, 22, 'N', 'c');
+        assertBug(nextMapBugs.get(6), 5, 13, 'N', 'd');
+    }
 
+    private void assertBug(Bug bug, int row, int column, char direction, char bugType) {
+        Assert.assertEquals(row, bug.getRow());
+        Assert.assertEquals(column, bug.getColumn());
+        Assert.assertEquals(direction, bug.getDirection());
+        Assert.assertEquals(bugType, bug.getBugType());
     }
 
     @Test
